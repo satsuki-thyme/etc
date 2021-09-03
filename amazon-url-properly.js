915557c15a5c83e02a3f018cc7a3d3d66672cacb
satsuki-thyme <satsuki.thyme@gmail.com>
2021-09-04 08:54:28 +0900

    正規表現をいじった

--- amazon-url-properly.js
+++ amazon-url-properly.js
@@ -1,6 +1,6 @@
 javascript:(function(){
 	s=location.href;
-	s="https://amazon.co.jp/dp/"+s.match(/\/([A-Z]+[0-9]+[A-Z0-9]*|[0-9]+)\//)[0].replace(/\//g, "");
+	s="https://amazon.co.jp/dp/"+s.match(/\/([A-Z0-9]{10,})\//)[0].replace(/\//g, "");
 	location.href=s;
 	navigator.clipboard.writeText(s);
 })();
