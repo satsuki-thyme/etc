ee95eec1b692c7fc8eba350f68029467157680f9
satsuki-thyme <satsuki.thyme@gmail.com>
2021-11-17 06:57:32 +0900

    商品コードが末尾だった際に動作しない問題を修正

--- amazon-url-properly.js
+++ amazon-url-properly.js
@@ -1,5 +1,5 @@
 javascript:(function(){
-	s="https://amazon.co.jp/dp/"+location.href.match(/\/([A-Z0-9]{10,})[/?]/)[0].replace(/[/?]/g, "");
+	s="https://amazon.co.jp/dp/"+location.href.match(/\/([A-Z0-9]{10,})(\/|\?|$)/)[0].replace(/[/?]/g, "");
 	location.href=s;
 	navigator.clipboard.writeText(s);
 })();
