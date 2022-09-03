1d4a39e5347a8f374955879f0e9ce51b37614685
satsuki-thyme <satsuki.thyme@gmail.com>
2022-09-04 06:10:46 +0900

    先読み・後読みに変更

--- amazon-url-properly.js
+++ amazon-url-properly.js
@@ -1,5 +1,5 @@
 javascript:(function(){
-	s = "https://amazon.co.jp/dp/" + location.href.match(/\/([A-Z0-9]{10,})(\/|\?|$)/)[0].replace(/[/?]/g, "");
+	s = "https://amazon.co.jp/dp/" + location.href.match(/(?<=\/)([A-Z0-9]{10,})(?=\/|\?|$)/)[0];
 	location.href = s;
 	navigator.clipboard.writeText(s);
 })();
