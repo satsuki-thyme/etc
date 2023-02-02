9afc01e017a8fdcc16a4599e5b4a618b310f052b
satsuki-thyme <satsuki.thyme@gmail.com>
2023-02-03 06:48:33 +0900

    大幅に変えた。前のはアホだった。

--- esc.js
+++ esc.js
@@ -1,34 +1,16 @@
 function esc(r) {
-  return r
-  .replace(/\//g, "\\/")
-  .replace(/\\/g, "\\\\")
-  .replace(/\^/g, "\\^")
-  .replace(/\$/g, "\\$")
-  .replace(/\*/g, "\\*")
-  .replace(/\+/g, "\\+")
-  .replace(/\?/g, "\\?")
-  .replace(/\./g, "\\.")
-  .replace(/\(/g, "\\(")
-  .replace(/\)/g, "\\)")
-  .replace(/\[/g, "\\[")
-  .replace(/\]/g, "\\]")
-  .replace(/\{/g, "\\{")
-  .replace(/\}/g, "\\}")
+  if (typeof r === "string" || r instanceof String) return p(r)
+  else if (Array.isArray(r)) return r.map(r => r = p(r))
+  else return r
+  function p(r) {
+    return r.replace(/(\/|\\|\^|\$|\*|\+|\?|\.|\(|\)|\[|\]|\{|\})/g, "\\$1")
+  }
 }
 function unEsc(r) {
-  return r
-  .replace(/\\\//g, "/")
-  .replace(/\\\\/g, "\\")
-  .replace(/\\\^/g, "^")
-  .replace(/\\\$/g, "$")
-  .replace(/\\\*/g, "*")
-  .replace(/\\\+/g, "+")
-  .replace(/\\\?/g, "?")
-  .replace(/\\\./g, ".")
-  .replace(/\\\(/g, "(")
-  .replace(/\\\)/g, ")")
-  .replace(/\\\[/g, "[")
-  .replace(/\\\]/g, "]")
-  .replace(/\\\{/g, "{")
-  .replace(/\\\}/g, "}")
+  if (typeof r === "string" || r instanceof String) return p(r)
+  else if (Array.isArray(r)) return r.map(r => r = p(r))
+  else return r
+  function p(r) {
+    return r.replace(/\\(\/|\\|\^|\$|\*|\+|\?|\.|\(|\)|\[|\]|\{|\})/g, "$1")
+  }
 }
