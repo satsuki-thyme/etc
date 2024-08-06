25a035cc5b7b3d2cb7c8a7c8f73b328bbc5411f6
satsuki-thyme <satsuki.thyme@gmail.com>
2024-08-07 07:11:04 +0900

    つまらない書き換え

--- esc.js
+++ esc.js
@@ -1,6 +1,6 @@
 function esc(r) {
   if (typeof r === "string" || r instanceof String) return p(r)
-  else if (Array.isArray(r)) return r.map(r => r = p(r))
+  else if (Array.isArray(r)) return r.map(r => p(r))
   else return r
   function p(r) {
     return r.replace(/(\/|\\|\^|\$|\*|\+|\?|\.|\(|\)|\[|\]|\{|\})/g, "\\$1")
@@ -8,7 +8,7 @@ function esc(r) {
 }
 function unEsc(r) {
   if (typeof r === "string" || r instanceof String) return p(r)
-  else if (Array.isArray(r)) return r.map(r => r = p(r))
+  else if (Array.isArray(r)) return r.map(r => p(r))
   else return r
   function p(r) {
     return r.replace(/\\(\/|\\|\^|\$|\*|\+|\?|\.|\(|\)|\[|\]|\{|\})/g, "$1")
